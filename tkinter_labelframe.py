535db91e3182bfca175c5293892aaeeb2204bca3
Joe W Mwangi <32553604+joemV@users.noreply.github.com>
2018-10-11 20:00:24 +0300

    Updates to code
    Some slight additions have been made to the code, it is a bit of a mess but that will be sorted out later.
    It currently has a working menu bar though incomplete, not all commands are functional
    Also there are now tabs and te UI has been split arbitrarily at the moment

--- tkinter_labelframe.py
+++ tkinter_labelframe.py
@@ -1,45 +1,83 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import scrolledtext
-import menubar_
+# import menubar_
 from tkinter import Menu
 
 win = tk.Tk()
 win.title("Second Tkinter GUI *LABEL FRAMES*")
 
-main_labelframe = ttk.LabelFrame(win, text="Label Frame",)
-main_labelframe.grid(column=0, row=0, padx=10, pady=5)
+menu_bar = Menu(win)
+win.config(menu=menu_bar)
+
+def quit_():
+	win.quit()
+	win.destroy()
+	exit()
+
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="New")
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=quit_)
+
+help_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Help", menu=help_menu)
+help_menu.add_command(label="Help")
+help_menu.add_command(label="About")
+
+tab_controller = ttk.Notebook(win)
+tab_1 = ttk.Frame(tab_controller)
+tab_controller.add(tab_1, text="Packages")
+tab_controller.pack()
+
+tab_2 = ttk.Frame(tab_controller)
+tab_controller.add(tab_2, text="Feedback")
+tab_controller.pack(fill="both", expand=1)
+
+package_labelframe = ttk.LabelFrame(tab_1, text="Package Management")
+package_labelframe.grid(column=0, row=0, padx=10, pady=5, sticky="WE")
+
+feedback_labelframe = ttk.LabelFrame(tab_2, text="Feedback and Response")
+feedback_labelframe.grid(column=0, row=0, padx=10, pady=5)
 
 def button_clicked():
 	submit_button.configure(text="Submitted")
-  
 
-name_label = ttk.Label(main_labelframe, text="Name")
+""" Feedback_labelframe Related Code"""
+name_label = ttk.Label(feedback_labelframe, text="Name")
 name_label.grid(column=0, row=0, sticky="W")
 
-country_label = ttk.Label(main_labelframe, text="Country")
+country_label = ttk.Label(feedback_labelframe, text="Country")
 country_label.grid(column=1, row=0)
 
 name_return = tk.StringVar()
-name_entry = ttk.Entry(main_labelframe, width=20, textvariable=name_return)
+name_entry = ttk.Entry(feedback_labelframe, width=25, textvariable=name_return)
 name_entry.grid(column=0, row=1, sticky="W")
 
-name_label = ttk.Label(main_labelframe, text="Name", width=15)
+name_label = ttk.Label(feedback_labelframe, text="Name")
 name_label.grid(column=0, row=0, sticky="W")
 
 choice_country = tk.StringVar()
-country_chosen = ttk.Combobox(main_labelframe, width=15,textvariable=choice_country)
-country_chosen["values"] = ("Kenya","Ugande","Rwanda","Tanzania","Burundi","Other")
-country_chosen.grid(column=1, row=0, sticky="W")
+country_chosen = ttk.Combobox(feedback_labelframe, width=20,textvariable=choice_country)
+country_chosen["values"] = ("Kenya","Uganda","Rwanda","Tanzania","Burundi","Other")
+country_chosen.grid(column=1, row=1, sticky="W")
 country_chosen.current(0)
 
-submit_button = ttk.Button(main_labelframe, text="Submit", width=10,
+submit_button = ttk.Button(feedback_labelframe, text="Submit", width=10,
 						   command=button_clicked)
-
 submit_button.grid(column=2, row=1, sticky="w")
 
+xlen=40
+ylen=4
+scrolled_textbar = scrolledtext.ScrolledText(feedback_labelframe, width=xlen,
+height=ylen, wrap=tk.WORD)
+scrolled_textbar.grid(columnspan=3, row=3, sticky="WE")
+
+""" package_labelframe Related Code """
 basic_pack = tk.IntVar()
-basic_pack_chckbtn = tk.Checkbutton(main_labelframe, text="Basic Package",variable=basic_pack, state="disabled")
+basic_pack_chckbtn = tk.Checkbutton(package_labelframe, text="Basic Package",
+variable=basic_pack, state="disabled")
 basic_pack_chckbtn.select()
 basic_pack_chckbtn.grid(column=0, row=2, sticky="W")
 '''
@@ -47,36 +85,24 @@ bsc = basic_pack.get()
 print(bsc)
 '''
 advanced_pack = tk.IntVar()
-advanced_pack_chckbtn = tk.Checkbutton(main_labelframe, text="Advanced Package",variable=advanced_pack)
+advanced_pack_chckbtn = tk.Checkbutton(package_labelframe, text="Advanced Package",
+variable=advanced_pack)
 advanced_pack_chckbtn.grid(column=1, row=2, sticky="W")
 '''
 adv = advanced_pack.get()
 print(adv)
 '''
 
-xlen=40
-ylen=4
-scrolled_textbar = scrolledtext.ScrolledText(main_labelframe, width=xlen,
-height=ylen, wrap=tk.WORD)
-scrolled_textbar.grid(columnspan=3, row=3)
+custom_pack = tk.IntVar()
+custom_package = tk.Checkbutton(package_labelframe, text="Custom Package",
+variable=custom_pack)
+custom_package.grid(column=2, row=2)
 
+content_labelframe = ttk.LabelFrame(package_labelframe, text="Package Contents")
+content_labelframe.grid(column=0, row=4, padx=10, pady=5)
 
-Menu_Bar = Menu(win)
-win.config(menu=Menu_Bar)
-
-def _exit():
-	win.quit()
-	exit()
-
-file_menu = Menu(Menu_Bar, tearoff=0)
-Menu_Bar.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="New")
-file_menu.add_separator()
-file_menu.add_command(label="Exit", command=_exit)
-
-help_menu = Menu(Menu_Bar, tearoff=0)
-Menu_Bar.add_cascade(label="Help", menu=help_menu)
-help_menu.add_command(label="About")
-help_menu.add_command(label="Help")
+if basic_pack.get() == 1:
+	ttk.Label(content_labelframe, text="Content 1").grid(column=0, row=0)
+	ttk.Label(content_labelframe, text="Content 2").grid(column=0, row=1)
 
 win.mainloop()
